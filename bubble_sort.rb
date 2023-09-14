016944e674397adc8b0394f985466663e6c495fd
Al <alpalma95@gmail.com>
2023-09-14 17:37:27 +0200

    Add optimization so the inner loop is shorter after every iteration

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,27 +1,31 @@
 SAMPLE = [4,3,78,2,0,2]
 
 def bubble_sort(arr = SAMPLE)
-
   is_sorted = false
+  max_index = arr.length - 1
 
   while !is_sorted do
     count = 0
-    arr.each_with_index do |num, i|
-      break if i + 1 == arr.length
 
-      if num > arr[i + 1]
+    (0..max_index).each do |i|
+      break if i + 1 > max_index
+
+      if arr[i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         count += 1
       end
+
     end
 
     if count == 0
       is_sorted = true
     end
+
+    max_index -= 1
   end
 
   print arr
 
 end
 
-bubble_sort
+bubble_sort [1, 86, 33, 99, 1, 3, 4, 6, 56, 69]
